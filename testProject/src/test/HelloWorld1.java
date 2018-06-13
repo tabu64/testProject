59a1f882765a2ad24b7dddf3d39b3c55f30be7e3
hn <hn2101520@gmail.com>
2018-06-13 22:16:25 +0900

    start

---
+++ testProject/src/test/HelloWorld1.java
@@ -0,0 +1,8 @@
+package test;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld123!!あ");
+	}
+}
---
+++ testProject/src/test/HelloWorld3.java
@@ -0,0 +1,8 @@
+package test;
+
+public class HelloWorld3 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld789!!い");
+	}
+}
